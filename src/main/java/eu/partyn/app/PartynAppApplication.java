55dc918a575365f197d36efb197cc16c3074318c
Karl-Andreas Meus <karl.andreas@meus.ee>
2024-04-18 12:52:49 +0300

    Initial commit

---
+++ src/main/java/eu/partyn/app/PartynAppApplication.java
@@ -0,0 +1,13 @@
+package eu.partyn.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartynAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartynAppApplication.class, args);
+	}
+
+}
